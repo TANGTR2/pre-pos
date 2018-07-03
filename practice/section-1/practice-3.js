6c745d1ed4ca61b38a7aea40e6d597585482c851
TANGTR2 <40784777+TANGTR2@users.noreply.github.com>
2018-07-03 22:01:23 +0800

    Update practice-3.js

--- practice/section-1/practice-3.js
+++ practice/section-1/practice-3.js
@@ -1,5 +1,20 @@
 'use strict';
 
 function collectSameElements(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  var cb=new Array();
+	var bsize=collectionB.value.length;
+	for(var i=0;i<bsize;i++)
+		cb[i]=(collectionB.value)[i];
+	var same=new Array();
+	var num=0;
+	 for(var i=0;i<collectionA.length;i++){
+		    for(var j=0;j<collectionB.length;j++){
+		      if(collectionA[i]==cb[j]){
+		    	 same[num]=collectionA[i];
+		    	 num++;
+		      } 
+		      else continue;
+		    }
+		  }
+  return same[num];
 }
