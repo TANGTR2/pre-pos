dc49560064f9df129d1fd1402623d5275eb424be
TANGTR2 <40784777+TANGTR2@users.noreply.github.com>
2018-07-03 22:08:29 +0800

    Update practice-3.js

--- practice/section-1/practice-3.js
+++ practice/section-1/practice-3.js
@@ -7,7 +7,7 @@ function collectSameElements(collectionA, objectB) {
 		cb[i]=(collectionB.value)[i];
 	var same=new Array();
 	var num=0;
-	 for(var i=0;i<collectionA.length;i++){
+	 for(var i=0;i<bsize;i++){
 		    for(var j=0;j<collectionB.length;j++){
 		      if(collectionA[i]==cb[j]){
 		    	 same[num]=collectionA[i];
