aa99e3efb72a567cf49b90eb311ecedfa409eba4
TANGTR2 <tang_hao1216@163.com>
2018-07-04 01:12:02 +0800

    p1-4

--- practice/section-1/practice-4.js
+++ practice/section-1/practice-4.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function collectSameElements(collectionA, objectB) {
+function collectSameElements(collectionA, collectionB) {
   var cb=new Array();
 	var bsize=collectionB.value.length;
 	for(var i=0;i<bsize;i++)
@@ -10,11 +10,11 @@ function collectSameElements(collectionA, objectB) {
 	 for(var i=0;i<collectionA.length;i++){
 		    for(var j=0;j<bsize;j++){
 		      if(collectionA[i].key==cb[j]){
-		    	 same[num]=collectionA[i];
+		    	 same[num]=collectionA[i].key;
 		    	 num++;
 		      } 
 		      else continue;
 		    }
 		  }
-  return same[num];
+  return same;
 }
