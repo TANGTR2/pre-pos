c69afd7ae99bcf5572ffeb0d7afa96c1455a2b9e
TANGTR2 <tang_hao1216@163.com>
2018-07-04 02:30:22 +0800

    p3-1

--- practice/section-3/practice-1.js
+++ practice/section-3/practice-1.js
@@ -1,5 +1,22 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+	var cb=new Array();
+	var bsize=objectB.value.length;
+	for(var i=0;i<bsize;i++)
+		cb[i]=(objectB.value)[i];
+	var same=new Array();
+	 for(var i=0;i<collectionA.length;i++){
+		    for(var j=0;j<bsize;j++){
+		      if(collectionA[i].key==cb[j]){
+				 same.push({key: collectionA[i].key,count: collectionA[i].count-1})
+				 break;
+		      } 
+		      else if(j==bsize-1){
+				 same.push({key: collectionA[i].key,count: collectionA[i].count})			 
+			  }
+			  else continue;
+		    }
+	}
+  return same;
 }
